8c23ccfa50f9ea3d5c189cacdd1d705a57af983d
Magali Baude <mag.baude@hotmail.fr>
2017-02-09 12:35:39 +0100

    ajout exercices dates

--- exercices/dates.php
+++ exercices/dates.php
@@ -31,11 +31,72 @@
 
   <h2>Exercice 4</h2>
   <!-- Afficher le timestamp du jour.-->
-  <p>Timestamp du jour : <?php  ?></p>
+  <p>Timestamp du jour : <?php echo time(); ?></p>
 
   <!-- Afficher le timestamp du mardi 2 août 2016 à 15h00.  -->
-  <p>Timestamp du mardi 2 août 2016 à 15h00 : <?php  ?></p>
 
+  <!-- mktime($heures, $minutes, $secondes, $mois, $jours, $annees); -->
+  <p>Timestamp du mardi 2 août 2016 à 15h00 : <?php echo mktime(15, 0, 0, 8, 2, 2016) ?></p>
+
+
+  <h2>Exercice 5</h2>
+  <!-- Afficher le nombre de jour qui sépare la date du jour avec le 16 mai 2016. -->
+  <p><strong>Methode date_diff() :</strong></p>
+  <?php
+  $datetime1 = date_create('now');
+  $datetime2 = date_create('2016-05-16');
+  $interval = date_diff($datetime2, $datetime1);
+  echo $interval->format('%R%a days');
+  ?>
+
+  <p><strong>Autre méthode (calcul) :</strong></p>
+  <?php
+  $dateDuJour = date('j-m-Y');
+  $dateCompare = "16-05-2016";
+  echo 'La date du jour : '.$dateDuJour.'</br>';
+
+  // On transforme les 2 dates en timestamp
+  $date1 = strtotime($dateDuJour);
+  $date2 = strtotime($dateCompare);
+
+  // On récupère la différence de timestamp entre les 2 précédents
+  $nbJoursTimestamp = $date1 - $date2;
+
+  // Pour convertir le timestamp (exprimé en secondes) en jours
+  // On sait que 1 heure = 60 secondes * 60 minutes et que 1 jour = 24 heures donc :
+  $nbJours = $nbJoursTimestamp/86400; // 86 400 = 60*60*24
+
+  echo "Nombre de jours depuis le 16 mai 2016 : ".$nbJours;
+  ?>
+
+
+  <h2>Exercice 6</h2>
+  <!-- Afficher le nombre de jour dans le mois de février de l'année 2016. -->
+
+  <!-- METHODE cal_days_in_month() -->
+  <!-- int cal_days_in_month ( int $calendar , int $month , int $year ) -->
+
+  <?php
+  $number = cal_days_in_month(CAL_GREGORIAN, 2, 2016);
+  echo '<p>Il y a '.$number.' jours dans le mois de février 2016</p>';
+  ?>
+
+
+  <h2>Exercice 7</h2>
+  <!-- Afficher la date du jour + 20 jours. -->
+  <?php
+  $plusVingt  = mktime(0, 0, 0, date('m'), date("d")+20, date('Y'));          //timestamp
+  $datePlusVingt = date('d / m / Y', $plusVingt);                             //convertit timestamp en date
+  echo '<p>Dans 20 jours nous serons le : '.$datePlusVingt.'</p>';
+  ?>
+
+  <h2>Exercice 8</h2>
+  <!-- Afficher la date du jour - 22 jours -->
+  <?php
+  $moinsVingtdeux  = mktime(0, 0, 0, date('m'), date("d")-22, date('Y'));     //timestamp
+  $dateMoinsVingtdeux = date('d / m / Y', $moinsVingtdeux);                   //convertit timestamp en date
+  echo '<p>Il y a 22 jours nous étions le : '.$dateMoinsVingtdeux.'</p>';
+  ?>
 
 </body>
 </html>
